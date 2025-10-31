62decce92012645879b8db0a3eb8ed11495fceca
Randson Marinho <randsonmarinho22@gmail.com>
2025-10-31 08:32:51 -0300

    Adição do .gitignore e limpeza dos arquivos desnecessários

--- src/InventoryService/Data/InventoryDbContext.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-public class InventoryDbContext : DbContext
-{
-    public InventoryDbContext(DbContextOptions<InventoryDbContext> options) : base(options) { }
-    public DbSet<Product> Products { get; set; } = null!;
-
-}
--- src/Shared/Events/OrderCreatedEvent.cs
+++
@@ -1,5 +0,0 @@
-using System;
-using System.Collections.Generic;
-
-public record OrderItemDto(Guid ProductId, int Quantity);
-public record OrderCreatedEvent(Guid OrderId, Guid CustomerId, List<OrderItemDto> Items, DateTime CreatedAt);
